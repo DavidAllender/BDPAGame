7568d1ac248fcc67685fb1924df2a59e10e6ba4a
David Allender <David.A.Allender@gmail.com>
2016-04-22 21:28:11 -0500

    Initial Commit

---
+++ src/SpaceShipControls.js
@@ -0,0 +1,42 @@
+/**
+ * Created by David Allender on 4/22/2016.
+ */
+Crafty.c("SpaceShipControls", {
+    required: "Motion, AngularMotion, Keyboard",
+    init: function () {
+        var k = Crafty.keys;
+        this.validKeys = [k.A, k.W, k.D, k.S, k.Q, k.E];
+        this.bind("KeyUp", this._keyup);
+        this.bind("KeyDown", this._keydown);
+        this.bind("EnterFrame", this._enterframe);
+    },
+    validKeys: [],
+    keys: [],
+    speed: 1,
+    rspeed: 1,
+    _keyup: function (e) {
+        this.keys.splice(this.keys.indexOf(e.key), 1);
+    },
+    _keydown: function (e) {
+        if(this.validKeys.indexOf(e.key) > -1) {
+            this.keys.push(e.key);
+        }
+    },
+    _enterframe: function (arg) {
+        var dt = arg.dt, k = Crafty.keys;
+        this.keys.forEach(function (key, i) {
+            if (this.isDown(key)) {
+                switch (key) {
+                    case k.A: this.vx -= this.speed * dt; break;
+                    case k.W: this.vy -= this.speed * dt; break;
+                    case k.D: this.vx += this.speed * dt; break;
+                    case k.S: this.vy += this.speed * dt; break;
+                    case k.Q: this.vrotation -= this.rspeed * dt; break;
+                    case k.E: this.vrotation += this.rspeed * dt; break;
+                }
+            } else {
+                this.keys.splice(i, 1);
+            }
+        }, this);
+    }
+});
---
+++ src/spacegame1.js
@@ -0,0 +1,79 @@
+window.onload = function () {
+    Crafty.init(window.innerWidth, window.innerHeight, document.getElementById('game'));
+    Crafty.background('#000000');
+
+    createScore();
+    createShootingControls();
+    createEnemy();
+    createShip();
+};
+
+var createShip = function(){
+    Crafty.e('PlayerShip, 2D, DOM, Color, SpaceShipControls')
+        .attr({x: 50, y: 60, w: 20, h: 20})
+        .color('#2ECC71')
+        .origin("center");
+};
+
+var createShootingControls = function(){
+    Crafty.e("Keyboard").bind("KeyDown", function(e){
+        var ship = Crafty("PlayerShip"), k = Crafty.keys, vx = ship.vx, vy = ship.vy, speed = 500;
+        var x = ship.x + ship.w/2, y = ship.y + ship.h/2;
+        if([k.LEFT_ARROW, k.UP_ARROW, k.RIGHT_ARROW, k.DOWN_ARROW].indexOf(e.key) > -1){
+
+            switch (e.key){
+                case k.LEFT_ARROW : vx -= speed; break;
+                case k.UP_ARROW : vy -= speed; break;
+                case k.RIGHT_ARROW : vx += speed; break;
+                case k.DOWN_ARROW : vy += speed; break;
+            }
+
+            createBullet(x, y, vx, vy);
+        }
+    });
+};
+
+var createEnemy = function(){
+    Crafty.e("Enemy, 2D, DOM, Color, Collision")
+        .attr({x: 500, y:500, w: 20, h:20})
+        .color("#0000FF");
+};
+
+var createBullet = function(x, y, vx, vy){
+    Crafty.e('Bullet, 2D, DOM, Color, Motion, Collision')
+        .attr({x: x, y:y, w: 5, h: 5, vx : vx, vy : vy})
+        .color('#E74C3C')
+        .onHit("Enemy", hitEnemy);
+};
+
+var createScore = function (){
+    Crafty.e("Score, DOM, 2D, Text")
+        .attr({ x: 20, y: 20, w: 100, h: 20})
+        .textColor("#FFFFFF")
+        .text("0 Points");
+};
+
+var score = 0;
+var hitEnemy = function(hitData){
+    this.destroy();
+    hitData[0].obj.destroy();
+    score = score + 1;
+    Crafty("Score").text(score + " Points");
+};
+
+
+
+
+/**
+ * Rouggh instruction steps:
+ * 1) Create a basic ship e.g. 2D, DOM, COLOr. Assign x y w h. Assign color
+ * 2) add spaceship controls. Notice that rotation is weird
+ * 3) set origin
+ * 4) bind to keydown and make it so that hitting space bar spawns a new object
+ * 5) rig up shooting to use the arrow keys.
+*/
+
+/**
+ * TODO:
+ * -Fix shooting while flying
+ */
